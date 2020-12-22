4cdb755136ab1d5ef13cc6c31193fdbf8c7eb0fa
etozhenerk <etozhenerk@yandex.ru>
2020-12-22 14:38:33 +0300

    Делегирование в index.js + работа видео плеера

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 import { radioPlayerInit } from "./radioPlayer.js";
 import { videoPlayerInit } from "./videoPlayer.js";
@@ -6,20 +6,31 @@ import { musicPlayerInit } from "./musicPlayer.js";
 
 const playerBtn = document.querySelectorAll(".player-btn"),
   playerBlock = document.querySelectorAll(".player-block"),
-  temp = document.querySelector(".temp");
+  temp = document.querySelector(".temp"),
+  btnSection = document.querySelector(".player");
 
-const deactivationPlayer = () => {
+const toggleTabContent = (index) => {
   temp.style.display = "none";
-  playerBtn.forEach(item => item.classList.remove("active"));
-  playerBlock.forEach(item => item.classList.remove("active"));
+  for (let i = 0; i < playerBlock.length; i++) {
+    if (index === i) {
+      playerBtn[i].classList.add("active");
+      playerBlock[i].classList.add("active");
+    } else {
+      playerBtn[i].classList.remove("active");
+      playerBlock[i].classList.remove("active");
+    }
+  }
 };
-
-playerBtn.forEach((btn, i) => {
-  btn.addEventListener("click", () => {
-    deactivationPlayer();
-    btn.classList.add("active");
-    playerBlock[i].classList.add("active");
-  });
+btnSection.addEventListener("click", (event) => {
+  let target = event.target;
+  target = target.closest(".player-btn");
+  if (target) {
+    playerBtn.forEach((item, i) => {
+      if (item === target) {
+        toggleTabContent(i);
+      }
+    });
+  }
 });
 
 radioPlayerInit();
--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,5 +1,73 @@
-'use strict';
+"use strict";
 
-export const videoPlayerInit = () =>{
-  console.log("video");
+export const videoPlayerInit = () => {
+  const videoPlayer = document.querySelector(".video-player"),
+    videoButtonPlay = document.querySelector(".video-button__play"),
+    videoTimePassed = document.querySelector(".video-time__passed"),
+    videoProgress = document.querySelector(".video-progress"),
+    videoTimeTotal = document.querySelector(".video-time__total"),
+    videoContainer = document.querySelector('.video-container');
+
+  const toggleIcon = () => {
+    if (videoPlayer.paused) {
+      videoButtonPlay.classList.remove("fa-pause");
+      videoButtonPlay.classList.add("fa-play");
+    } else {
+      videoButtonPlay.classList.add("fa-pause");
+      videoButtonPlay.classList.remove("fa-play");
+    }
+  };
+
+  const togglePlay = () => {
+    if (videoPlayer.paused) {
+      videoPlayer.play();
+    } else {
+      videoPlayer.pause();
+    }
+    toggleIcon();
+  };
+
+  const stopPlay = () => {
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+    toggleIcon();
+  };
+
+  const addZero = (n) => (n < 10 ? `0${n}` : n);
+
+  const timeUpdate = () => {
+    const currentTime = videoPlayer.currentTime,
+      duration = videoPlayer.duration;
+
+    videoProgress.value = (currentTime / duration) * 100;
+
+    let minutePassed = Math.floor(currentTime / 60),
+      secondsPassed = Math.floor(currentTime % 60),
+      minuteTotal = Math.floor(duration / 60),
+      secondsTotal = Math.floor(duration % 60);
+
+    videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`;
+    videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`;
+  };
+
+  const progressChange = () => {
+    const duration = videoPlayer.duration,
+      value = videoProgress.value;
+
+    videoPlayer.currentTime = (value * duration) / 100;
+  };
+
+  const turnOnPlayer = (event) => {
+    const target = event.target;
+    if(target.matches(".video-player")|| target.matches('.video-button__play')){
+      togglePlay();
+    }else if(target.matches(".video-button__stop")){
+      stopPlay();
+    }
+  };
+
+
+  videoContainer.addEventListener('click', turnOnPlayer);
+  videoPlayer.addEventListener("timeupdate", timeUpdate);
+  videoProgress.addEventListener("change", progressChange);
 };
